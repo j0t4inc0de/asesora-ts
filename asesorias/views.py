d74a50f1b2c912a6fe83a48e910e943c9168dfcd
j0t4inc0de <juan.erices04@inacapmail.cl>
2026-04-22 21:20:04 -0400

    templates

--- asesorias/views.py
+++ asesorias/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Servicio
 # Create your views here.
+
+def home(request):
+    servicios = Servicio.objects.all()
+    return render(request, 'asesorias/templates/index.html', {'servicios': servicios})
+
