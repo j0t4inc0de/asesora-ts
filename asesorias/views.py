57716db2e2f2d01b740d5ef02d4d6c6d5fc7acb1
j0t4inc0de <juan.erices04@inacapmail.cl>
2026-04-23 21:55:38 -0400

    se bloquean las fechas pasadas para no elegirlas

--- asesorias/views.py
+++ asesorias/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import redirect, render
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import date
 from .models import Servicio
 from .forms import FormularioDiagnostico
-# Create your views here.
-
 
 def home(request):
     servicios = Servicio.objects.all()
     form = FormularioDiagnostico()
-    return render(request, 'index.html', {'servicios': servicios, 'form': form})
-
+    hoy = date.today().strftime('%Y-%m-%d')
+    
+    return render(request, 'index.html', {
+        'servicios': servicios, 
+        'form': form, 
+        'hoy': hoy
+    })
 
 def agendar_cita(request):
     if request.method == 'POST':
@@ -22,33 +26,28 @@ def agendar_cita(request):
             asunto = f"Nuevo Formulario de Contacto: {datos['nombre']}"
             mensaje = f"""
             Has recibido una nueva solicitud en AsesoraTS Chile:
-            
             - Nombre: {datos['nombre']}
             - RUT: {datos['rut']}
-            - Teléfono: {datos['telefono']} (Llamar para agendar)
+            - Teléfono: {datos['telefono']}
             - Email: {datos['email']}
             - Motivo: {datos['motivo_consulta']}
             - Fecha sugerida: {datos['fecha_deseada'] if datos['fecha_deseada'] else 'No especificada'}
             """
-
-            # Enviamos el correo
             send_mail(
-                asunto,
-                mensaje,
-                settings.EMAIL_HOST_USER,
-                [settings.EMAIL_HOST_USER],
-                fail_silently=False,
+                asunto, mensaje, settings.EMAIL_HOST_USER,
+                [settings.EMAIL_HOST_USER], fail_silently=False,
             )
             return redirect('confirmacion_solicitud')
-    else:
-        servicios = Servicio.objects.all()
-        return render(request, 'index.html', {
-            'form': form,
-            'servicios': servicios
-        })
+        else:
+            servicios = Servicio.objects.all()
+            hoy = date.today().strftime('%Y-%m-%d')
+            return render(request, 'index.html', {
+                'form': form,
+                'servicios': servicios,
+                'hoy': hoy
+            })
 
     return redirect('home')
-
 
 def confirmacion_solicitud(request):
     return render(request, 'confirmacion.html')
