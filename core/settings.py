f2c5f632e1b1e3b1d0c614a3c3d7d1de16a6ab76
j0t4inc0de <juan.erices04@inacapmail.cl>
2026-05-22 05:17:30 -0400

    se añade configuración de almacenamiento para archivos estáticos y medios

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,14 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
